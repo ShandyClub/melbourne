553388fb999bc8d5751138df9896579bd9a07f33
lukehedger <luke.hedger@gmail.com>
2018-12-06 17:12:08 +1100

    2 tabs

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Title = styled.h1`
-    color: red;
-    font-size: 50px;
+  color: red;
+  font-size: 50px;
 `
 
 export default () => (
-    <>
-        <Head>
-            <title>My page title</title>
-            <meta
-                name="viewport"
-                content="initial-scale=1.0, width=device-width"
-                key="viewport"
-            />
-        </Head>
-        <Head>
-            <meta
-                name="viewport"
-                content="initial-scale=1.2, width=device-width"
-                key="viewport"
-            />
-        </Head>
-        <Title>Shandy Club ~ Melbourne</Title>
-    </>
+  <>
+    <Head>
+      <title>My page title</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
+      />
+    </Head>
+    <Head>
+      <meta
+        name="viewport"
+        content="initial-scale=1.2, width=device-width"
+        key="viewport"
+      />
+    </Head>
+    <Title>Shandy Club ~ Melbourne</Title>
+  </>
 )
